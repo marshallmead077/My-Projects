fa79c554a479e77879a71934285dec41fc45daf4
marshallmead077 <marshallmead.mm39@gmail.com>
2022-12-19 00:03:42 -0600

    added java practice file

---
+++ javatutorial/index.js
@@ -0,0 +1,25 @@
+//variabels//
+let num = 4
+console.log(num)
+let user = "marshall's account"
+console.log(user)
+num = 9
+console.log(num)
+
+
+//constants//
+let radius = 5
+const pi = 3.14
+let area
+radius = 6
+
+area = pi * radius *radius
+console.log(area)
+
+//data types//
+let data = 8 //number//
+let text = "name" //string//
+
+//numbers//
+let num1 = 0xf
+console.log(num1);
