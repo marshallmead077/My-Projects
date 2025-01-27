014af1a7cfcc670627cada1e11a01c78ab48ee8d
marshallmead077 <marshallmead.mm39@gmail.com>
2025-01-27 03:18:00 -0600

    UPDATE

--- movieApp/Frontend/src/App.jsx
+++ movieApp/Frontend/src/App.jsx
@@ -1,9 +1,16 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Home from "./Pages/Favorites";
 import Home from "./Pages/Home";
 function App() {
  return (
   <>
-  <Home />
+ <main className="main-content">
+  <Routes>
+    <Route path="/" element= {<Home />}/>
+    <Route path="/Favorites" element= {<Favorites />}/>
+  </Routes>
+ </main>
   </>
   );
 }
---
+++ movieApp/Frontend/src/Pages/favorites.jsx
@@ -0,0 +1,7 @@
+function Favorite(){
+    return <div className="favorites-empty">
+        <p>Start your Favorites list now!</p>
+    </div>
+}
+
+export default Favorite
