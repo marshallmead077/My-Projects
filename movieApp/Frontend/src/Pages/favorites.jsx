30912fb293802fabb3479dab42a9069abcca0501
marshallmead077 <marshallmead.mm39@gmail.com>
2025-01-28 01:13:33 -0600

    update

--- movieApp/Frontend/src/Pages/favorites.jsx
+++
@@ -1,8 +0,0 @@
-function Favorite(){
-    return <div className="favorites-empty">
-        <h1>No favorites saved</h1>
-        <p>Start your Favorites list now!</p>
-    </div>
-}
-
-export default Favorite
